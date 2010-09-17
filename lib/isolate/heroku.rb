ac2a6f888ff5f3c421babb067d44b97010d1e282
John Barnette <jbarnette@gmail.com>
2010-09-17 07:14:29 -0700

    Kill trailing whitespace.

--- lib/isolate/heroku.rb
+++ lib/isolate/heroku.rb
@@ -9,14 +9,14 @@ unless ENV.keys.any? { |k| /^heroku/i =~ k }
 
       gems  = [entry.name]
       gems << "--version '#{entry.requirement}'"
-      
+
       if entry.options[:source]
         gems << "--source #{entry.options[:source]}"
       end
-      
+
       f.puts gems.join(" ")
     end
   end
-  
+
   STDERR.puts "WARNING: isolate-heroku has modified .gems, please commit to git." if /\.gems/ =~ `git status`
 end
